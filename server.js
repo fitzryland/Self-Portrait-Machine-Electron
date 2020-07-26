41761387dd970e1dabe8902b48f1cc5888cd8486
Fitz Ryland <fitzryland@gmail.com>
2020-07-26 15:46:41 -0700

    very basic electron app with webpack and react

---
+++ server.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+require('electron-reload')(__dirname, {
+    electron: require(`${__dirname}/node_modules/electron`)
+});
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        KABLAMOOOOOOO 3:43
+      </div>
+    )
+  }
+}
+
+export default App
